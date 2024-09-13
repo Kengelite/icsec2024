f9cd480c65e61c91bebdb20d1f109571c644e7be
Elite <elite@192.168.0.153>
2024-09-13 21:27:12 +0700

    13 September 2024 (Round 2) Notifiedก

--- keynote.php
+++ keynote.php
@@ -14,161 +14,81 @@
     <link rel="stylesheet" href="style.css">
 
     <style>
-        * {
-            color: black;
+    * {
+        color: black;
+    }
+
+    .text {
+        color: rgb(255, 255, 255);
+        font-size: 15px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        /* text-align: center; */
+        padding: 20px;
+    }
+
+    @media (max-width: 1200px) {
+        .carousel-caption h2 {
+            font-size: 1.75rem;
         }
 
-        .text {
-            color: rgb(255, 255, 255);
-            font-size: 15px;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            /* text-align: center; */
-            padding: 20px;
+        .carousel-caption h5 {
+            font-size: 1rem;
         }
-        @media (max-width: 1200px) {
-    .carousel-caption h2 {
-        font-size: 1.75rem;
     }
-    .carousel-caption h5 {
-        font-size: 1rem;
-    }
-}
 
-@media (max-width: 992px) {
-    .carousel-caption h2 {
-        font-size: 1.5rem;
-    }
-    .carousel-caption h5 {
-        font-size: 0.875rem;
-    }
-}
+    @media (max-width: 992px) {
+        .carousel-caption h2 {
+            font-size: 1.5rem;
+        }
 
-@media (max-width: 768px) {
-    .carousel-caption h2 {
-        font-size: 1.25rem;
+        .carousel-caption h5 {
+            font-size: 0.875rem;
+        }
     }
-    .carousel-caption h5 {
-        font-size: 0.75rem;
-    }
-}
 
-@media (max-width: 576px) {
-    .carousel-caption h2 {
-        font-size: 1rem;
+    @media (max-width: 768px) {
+        .carousel-caption h2 {
+            font-size: 1.25rem;
+        }
+
+        .carousel-caption h5 {
+            font-size: 0.75rem;
+        }
     }
-    .carousel-caption h5 {
-        font-size: 0.625rem;
+
+    @media (max-width: 576px) {
+        .carousel-caption h2 {
+            font-size: 1rem;
+        }
+
+        .carousel-caption h5 {
+            font-size: 0.625rem;
+        }
     }
-}
-@media (max-width: 768px) {
-    .carousel-caption h5 span:nth-child(2) {
-        display: block;
+
+    @media (max-width: 768px) {
+        .carousel-caption h5 span:nth-child(2) {
+            display: block;
+        }
     }
-}
     </style>
 
-</head><body data-bs-spy="scroll" data-bs-target=".navbar">
+</head>
 
-<?php
+<body data-bs-spy="scroll" data-bs-target=".navbar">
+
+    <?php
 include './menu.php';
 ?>
 
-<!-- HERO -->
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
-                aria-label="Slide 4"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="./assets/images/silde/bg1.png" class="d-block w-100" alt="...">
-                <div
-                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
-                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
-                        AND ENGINEERING CONFERENCE 2024</h2> <br>
-                    <h5 style="color: white;">6-8 November 2024</h5>
-                    <h5 style="color: white;">
-                        Paper submission deadline:
-                        <span
-                            style="color: #FF0000; text-decoration: line-through; background-color: rgba(255, 255, 255, 0.7);">12
-                            july 2024</span><span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
-                            extended 26 july
-                            2024</span>
-                    </h5>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="./assets/images/silde/bg2.png" class="d-block w-100" alt="...">
-                <div
-                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
-                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
-                        AND ENGINEERING CONFERENCE 2024</h2> <br>
-                    <h5 style="color: white;">6-8 November 2024</h5>
-                    <h5 style="color: white;">
-                        Paper submission deadline:
-                        <span
-                            style="color: #FF0000; text-decoration: line-through; background-color: rgba(255, 255, 255, 0.7);">12
-                            july 2024</span><span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
-                            extended 26 july
-                            2024</span>
-                    </h5>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="./assets/images/silde/bg3.png" class="d-block w-100" alt="...">
-                <div
-                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
-                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
-                        AND ENGINEERING CONFERENCE 2024</h2> <br>
-                    <h5 style="color: white;">6-8 November 2024</h5>
-                    <h5 style="color: white;">
-                        Paper submission deadline:
-                        <span
-                            style="color: #FF0000; text-decoration: line-through; background-color: rgba(255, 255, 255, 0.7);">12
-                            july 2024</span><span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
-                            extended 26 july
-                            2024</span>
-                    </h5>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="./assets/images/silde/bg4.png" class="d-block w-100" alt="...">
-                <div
-                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
-                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
-                        AND ENGINEERING CONFERENCE 2024</h2> <br>
-                    <h5 style="color: white;">6-8 November 2024</h5>
-                    <h5 style="color: white;">
-                        Paper submission deadline:
-                        <span
-                            style="color: #FF0000; text-decoration: line-through; background-color: rgba(255, 255, 255, 0.7);">12
-                            july 2024</span><span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
-                            extended 26 july
-                            2024</span>
-                    </h5>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+    <!-- HERO -->
+    <?php
+   include './slide.php';
+   ?>
+
 
     <div class="container">
         <div class="row">
@@ -190,11 +110,13 @@ include './menu.php';
                                 <div class="text">
                                     <h5 style="color: white;">Professor Dusit Niyato</h5>
                                     <h5 style="color: white;">Ph.D., IEEE Fellow, IET Fellow</h5><br>
-                                    <p style="color: white;">President's Chair Professor in Computer Science and Engineering School of
+                                    <p style="color: white;">President's Chair Professor in Computer Science and
+                                        Engineering School of
                                         Computer
                                         Science and Engineering (SCSE), Nanyang Technological University, Singapore.
                                     </p>
-                                    <a href="https://personal.ntu.edu.sg/dniyato/" class="btn btn-primary" target="_blank">View Profile</a>
+                                    <a href="https://personal.ntu.edu.sg/dniyato/" class="btn btn-primary"
+                                        target="_blank">View Profile</a>
                                 </div>
                             </div>
 
@@ -209,10 +131,12 @@ include './menu.php';
                                 <div class="text">
                                     <h5 style="color: white;">Professor Thomas Bäck</h5>
                                     <h5 style="color: white;">Ph.D., IEEE Fellow</h5><br>
-                                    <p style="color: white;">Leiden Institute of Advanced Computer Science (LIACS), Leiden University,
+                                    <p style="color: white;">Leiden Institute of Advanced Computer Science (LIACS),
+                                        Leiden University,
                                         Netherlands.
                                     </p>
-                                    <a href="https://www.universiteitleiden.nl/en/staffmembers/thomas-back#tab-2" class="btn btn-primary" target="_blank">View Profile</a>
+                                    <a href="https://www.universiteitleiden.nl/en/staffmembers/thomas-back#tab-2"
+                                        class="btn btn-primary" target="_blank">View Profile</a>
                                     <a href="keynote_abstract.php" class="btn btn-primary">Abstract</a>
                                 </div>
                             </div>
@@ -231,7 +155,8 @@ include './menu.php';
                                     <p style="color: white;">Ludwig-Maximilians Universität München.
                                     </p>
                                     <!-- keynot_ab_gitta.php -->
-                                    <a href="https://www.ai.math.uni-muenchen.de/members/professor/kutyniok/index.html" class="btn btn-primary" target="_blank">View Profile</a>
+                                    <a href="https://www.ai.math.uni-muenchen.de/members/professor/kutyniok/index.html"
+                                        class="btn btn-primary" target="_blank">View Profile</a>
                                     <a href="keynot_ab_gitta.php" class="btn btn-primary">Abstract</a>
                                 </div>
                             </div>
@@ -257,23 +182,23 @@ include './menu.php';
     <script src="./assets/js/main.js"></script>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const imageCards = document.querySelectorAll('.image-card');
+    document.addEventListener('DOMContentLoaded', function() {
+        const imageCards = document.querySelectorAll('.image-card');
 
-            imageCards.forEach(card => {
-                const overlay = card.querySelector('.overlay');
-                const text = card.querySelector('.text');
+        imageCards.forEach(card => {
+            const overlay = card.querySelector('.overlay');
+            const text = card.querySelector('.text');
 
-                card.addEventListener('mouseover', () => {
-                    const textHeight = text.offsetHeight;
-                    overlay.style.height = `${textHeight}px`;
-                });
+            card.addEventListener('mouseover', () => {
+                const textHeight = text.offsetHeight;
+                overlay.style.height = `${textHeight}px`;
+            });
 
-                card.addEventListener('mouseleave', () => {
-                    overlay.style.height = '0';
-                });
+            card.addEventListener('mouseleave', () => {
+                overlay.style.height = '0';
             });
         });
+    });
     </script>
 </body>
 
---
+++ slide.php
@@ -0,0 +1,108 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./assets/images/silde/bg1.png" class="d-block w-100" alt="...">
+                <div
+                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
+                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
+                        AND ENGINEERING CONFERENCE 2024</h2> <br>
+                    <h5 style="color: white;">6-8 November 2024</h5>
+                    <h5 style="color: white;">
+                        Camera Ready Submission Deadline :
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                             30 September
+                            2024</span>
+                    </h5>
+                    <h5 style="color: white;">
+                    Registration: 
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                        16 August – 31 October 
+                            2024</span>
+                    </h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./assets/images/silde/bg2.png" class="d-block w-100" alt="...">
+                <div
+                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
+                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
+                        AND ENGINEERING CONFERENCE 2024</h2> <br>
+                    <h5 style="color: white;">6-8 November 2024</h5>
+                    <h5 style="color: white;">
+                        Camera Ready Submission Deadline :
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                             30 September
+                            2024</span>
+                    </h5>
+                    <h5 style="color: white;">
+                    Registration: 
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                        16 August – 31 October 
+                            2024</span>
+                    </h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./assets/images/silde/bg3.png" class="d-block w-100" alt="...">
+                <div
+                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
+                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
+                        AND ENGINEERING CONFERENCE 2024</h2> <br>
+                    <h5 style="color: white;">6-8 November 2024</h5>
+                    <h5 style="color: white;">
+                        Camera Ready Submission Deadline :
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                             30 September
+                            2024</span>
+                    </h5>
+                    <h5 style="color: white;">
+                    Registration: 
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                        16 August – 31 October 
+                            2024</span>
+                    </h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./assets/images/silde/bg4.png" class="d-block w-100" alt="...">
+                <div
+                    class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
+                    <h2 style="color: white;">THE 28<sup style="color: white;">TH</sup> INTERNATIONAL COMPUTER SCIENCE
+                        AND ENGINEERING CONFERENCE 2024</h2> <br>
+                    <h5 style="color: white;">6-8 November 2024</h5>
+                    <h5 style="color: white;">
+                        Camera Ready Submission Deadline :
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                             30 September
+                            2024</span>
+                    </h5>
+                    <h5 style="color: white;">
+                    Registration: 
+                        <span style="color: #FF0000; background-color: rgba(255, 255, 255, 0.7);">
+                        16 August – 31 October 
+                            2024</span>
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
