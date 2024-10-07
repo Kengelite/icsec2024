b62d3b36ec1288cd0bbe08e430ee4aec784e5ec7
Elite <elite@Elites-MacBook-Pro.local>
2024-10-07 14:20:21 +0700

    invited new person paisarn

--- invited.php
+++ invited.php
@@ -13,94 +13,96 @@
     <link rel="icon" href="./assets/images/organnized/ICSEC.png" type="image/gif" sizes="16x16">
 </head>
 <style>
-    * {
-        color: black;
-    }
+* {
+    color: black;
+}
 
-    .section-title {
-        text-align: center;
-    }
+.section-title {
+    text-align: center;
+}
 
-    .left-align {
-        text-align: left;
-    }
+.left-align {
+    text-align: left;
+}
 
-    .image-bio-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        /* Center vertically */
-        align-items: center;
-        /* Center horizontally */
-        text-align: center;
-        /* Center text */
-        height: 100%;
-        /* Ensure container takes up full height */
-    }
+.image-bio-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    /* Center vertically */
+    align-items: center;
+    /* Center horizontally */
+    text-align: center;
+    /* Center text */
+    height: 100%;
+    /* Ensure container takes up full height */
+}
 
-    .image-bio-container h4 {
-        font-size: 18px;
-        /* Adjust the size as needed */
-    }
+.image-bio-container h4 {
+    font-size: 18px;
+    /* Adjust the size as needed */
+}
 
-    /* Ensure proper alignment of the biography and the name */
-    .biography {
-        margin-top: 20px;
-        /* Adjust this value as needed */
-    }
+/* Ensure proper alignment of the biography and the name */
+.biography {
+    margin-top: 20px;
+    /* Adjust this value as needed */
+}
 
-    .image-container {
-        width: 100%;
-        /* Set the desired width */
-        height: 100%;
-        /* Set the desired height */
-        overflow: hidden;
-        /* Hide the overflowing part of the image */
-        display: flex;
-        /* Use flexbox */
-        justify-content: center;
-        /* Center horizontally */
-        align-items: center;
-        /* Center vertically */
-        margin: auto;
-        /* Center the container itself */
-    }
+.image-container {
+    width: 100%;
+    /* Set the desired width */
+    height: 100%;
+    /* Set the desired height */
+    overflow: hidden;
+    /* Hide the overflowing part of the image */
+    display: flex;
+    /* Use flexbox */
+    justify-content: center;
+    /* Center horizontally */
+    align-items: center;
+    /* Center vertically */
+    margin: auto;
+    /* Center the container itself */
+}
 
-    .image-container img {
-        max-width: 100%;
-        /* Ensure the image retains its aspect ratio */
-        max-height: 100%;
-        /* Ensure the image retains its aspect ratio */
-        object-fit: cover;
-        /* Crop the image to cover the container */
-    }
+.image-container img {
+    max-width: 100%;
+    /* Ensure the image retains its aspect ratio */
+    max-height: 100%;
+    /* Ensure the image retains its aspect ratio */
+    object-fit: cover;
+    /* Crop the image to cover the container */
+}
 
-    .small-square {
-        display: inline-block;
-        width: 50px;
-        height: 5px;
-        background-color: #5054d4;
-        vertical-align: middle;
-        /* Adjusts vertical alignment to be centered with text */
-        margin-right: 5px;
-        /* Adds space between the square and the text */
-    }
+.small-square {
+    display: inline-block;
+    width: 50px;
+    height: 5px;
+    background-color: #5054d4;
+    vertical-align: middle;
+    /* Adjusts vertical alignment to be centered with text */
+    margin-right: 5px;
+    /* Adds space between the square and the text */
+}
 
-    .text {
-        display: inline-block;
-        width: 100%;
-        text-align: justify;
-    }
+.text {
+    display: inline-block;
+    width: 100%;
+    text-align: justify;
+}
 
-    .text {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-    }
-    @media (max-width: 1200px) {
+.text {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+@media (max-width: 1200px) {
     .carousel-caption h2 {
         font-size: 1.75rem;
     }
+
     .carousel-caption h5 {
         font-size: 1rem;
     }
@@ -110,6 +112,7 @@
     .carousel-caption h2 {
         font-size: 1.5rem;
     }
+
     .carousel-caption h5 {
         font-size: 0.875rem;
     }
@@ -119,6 +122,7 @@
     .carousel-caption h2 {
         font-size: 1.25rem;
     }
+
     .carousel-caption h5 {
         font-size: 0.75rem;
     }
@@ -128,16 +132,19 @@
     .carousel-caption h2 {
         font-size: 1rem;
     }
+
     .carousel-caption h5 {
         font-size: 0.625rem;
     }
 }
+
 @media (max-width: 768px) {
     .carousel-caption h5 span:nth-child(2) {
         display: block;
     }
 }
 </style>
+
 <body data-bs-spy="scroll" data-bs-target=".navbar">
 
     <?php
@@ -243,7 +250,8 @@
                 <div class="col-md-12" data-aos="fade-up" data-aos-delay="300">
                     <h5><span style="font-size: 12px;">►</span> Biography</h5>
                     <div class="text">
-                        <p> <span class="ms-4"> He </span> is a Professor at the School of Information Science & Technology, Vidyasirimedhi Institute
+                        <p> <span class="ms-4"> He </span> is a Professor at the School of Information Science &
+                            Technology, Vidyasirimedhi Institute
                             of
                             Science & Technology (VISTEC), located in Rayong, Thailand. In addition to his primary role,
                             he
@@ -255,13 +263,15 @@
                         </p>
 
 
-                        <p> <span class="ms-4"> As </span> author or co-author, he has published over 120 publications in journals (e.g., Nature
+                        <p> <span class="ms-4"> As </span> author or co-author, he has published over 120 publications
+                            in journals (e.g., Nature
                             Physics, Nature Machine Intelligence, IEEE Transactions on Cybernetics, IEEE Transactions on
                             Neural Networks and Learning Systems, IEEE Robotics and Automation Letters) and conferences
                             (e.g., IROS, ICRA) and his articles have been cited in total more than 3100 citations. His
                             H-index is 29 (from google scholar).</p>
 
-                        <p><span class="ms-4"> He </span> has been the Principal Investigator (PI) or co-Principal Investigator (co-PI) on more
+                        <p><span class="ms-4"> He </span> has been the Principal Investigator (PI) or co-Principal
+                            Investigator (co-PI) on more
                             than 10
                             funded projects, including those funded by EU Horizon 2020, Human Frontier Science Program
                             (HFSP), and Doctoral Networks - Marie Skłodowska-Curie Actions. Currently, he serves as an
@@ -272,7 +282,8 @@
                             (2020,2021,2022). He also serves as the editorial board of the Scientific Reports and
                             Journal of
                             the Royal Society Interface.</p>
-                        <p> <span class="ms-4"> The </span> central goal of his research is to understand “how brain-like mechanisms and
+                        <p> <span class="ms-4"> The </span> central goal of his research is to understand “how
+                            brain-like mechanisms and
                             biomechanics
                             can be realized in robots so they can become more intelligent like living creatures?”.
                             According
@@ -290,15 +301,102 @@
         </div>
     </section>
 
+
+    <section id="another-about" class="ANOTHER-ABOUT section-padding">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="50">
+                    <h1 class="display-4 fw-semibold">Revolutionizing Healthcare with Digital Twins: The Power of
+                        Generative AI Integration</h1>
+                    <h5><span class="small-square"></span></h5>
+                    <div class="line"></div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-4 text-center" data-aos="fade-right" data-aos-delay="100">
+                    <img src="./assets/images/invited/Paisarn.jpg" class="img-fluid " alt="Poramate Manoonpong">
+                    <h6 class="mt-3">Paisarn Muneesawang, Ph.D.</h6>
+                    <p>Professor of Computer Engineering,  <br> Mahidol University</p>
+                </div>
+                <div class="col-md-8" data-aos="fade-left" data-aos-delay="200">
+                    <h5><span style="font-size: 12px;">► </span>Abstract</h5>
+                    <p class="text">This session explores the transformative potential of Digital Twin technology in
+                        healthcare, enhanced by the integration of Generative AI. We will delve into how Digital
+                        Twins—virtual replicas of physical entities—can revolutionize patient care by simulating
+                        real-time health scenarios and predicting outcomes. By leveraging Generative AI, these systems
+                        can generate comprehensive health reports, interpret complex datasets, and provide actionable
+                        insights. This talk will cover key use cases, including predictive healthcare models, patient
+                        monitoring, and the application of AI to optimize treatments and diagnoses, driving a new era of
+                        precision medicine.</p>
+
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-12" data-aos="fade-up" data-aos-delay="300">
+                    <!-- <h5><span style="font-size: 12px;">►</span> Biography</h5>
+                    <div class="text">
+                        <p> <span class="ms-4"> He </span> is a Professor at the School of Information Science &
+                            Technology, Vidyasirimedhi Institute
+                            of
+                            Science & Technology (VISTEC), located in Rayong, Thailand. In addition to his primary role,
+                            he
+                            holds an ancillary academic position as a Professor of Biorobotics at the University of
+                            Southern
+                            Denmark (SDU), Denmark. Furthermore, he serves as the head of the Research Center for
+                            Advanced
+                            Robotics and Intelligent Automation at VISTEC.
+                        </p>
+
+
+                        <p> <span class="ms-4"> As </span> author or co-author, he has published over 120 publications
+                            in journals (e.g., Nature
+                            Physics, Nature Machine Intelligence, IEEE Transactions on Cybernetics, IEEE Transactions on
+                            Neural Networks and Learning Systems, IEEE Robotics and Automation Letters) and conferences
+                            (e.g., IROS, ICRA) and his articles have been cited in total more than 3100 citations. His
+                            H-index is 29 (from google scholar).</p>
+
+                        <p><span class="ms-4"> He </span> has been the Principal Investigator (PI) or co-Principal
+                            Investigator (co-PI) on more
+                            than 10
+                            funded projects, including those funded by EU Horizon 2020, Human Frontier Science Program
+                            (HFSP), and Doctoral Networks - Marie Skłodowska-Curie Actions. Currently, he serves as an
+                            associate editor of IEEE Robotics and Automation Letters, Frontiers in Neuroscience
+                            (Neurorobotics), and Adaptive Behavior (SAGE), a guest associate editor of Frontiers in
+                            Robotics
+                            and AI, and an associate editor for IEEE International Conference on Soft Robotics
+                            (2020,2021,2022). He also serves as the editorial board of the Scientific Reports and
+                            Journal of
+                            the Royal Society Interface.</p>
+                        <p> <span class="ms-4"> The </span> central goal of his research is to understand “how
+                            brain-like mechanisms and
+                            biomechanics
+                            can be realized in robots so they can become more intelligent like living creatures?”.
+                            According
+                            to this, his team has developed bio-inspired behaving robots with general bio-inspired
+                            machine
+                            learning methods and could show that these robots can acquire complex behaviors with
+                            learning
+                            and adaptation. In addition to this, his team also focuses on transferring biomechanical and
+                            neural developments of robots to other real-world applications, like inspection, healthcare,
+                            industry, service.</p>
+                        .
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+
     <!-- Optional JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
-        AOS.init({
-            duration: 800,
-            once: true
-        });
+    AOS.init({
+        duration: 800,
+        once: true
+    });
     </script>
 
 
@@ -307,7 +405,7 @@
     ?>
 
 
-   
+
 </body>
 
 </html>
