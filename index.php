f490e716ab63c8c62632e94950fab8e875da39b9
Elite <elite@Elites-MacBook-Pro.local>
2024-11-07 18:13:42 +0700

    update

--- index.php
+++ index.php
@@ -85,19 +85,27 @@
    include './slide.php';
    ?>
 
-
+    E
+    <!-- Event Photos -->
     <section id="event" class="section-padding">
         <div class="container">
             <div class="row">
                 <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="50">
                     <div class="section-title">
                         <h1 class="display-4 fw-semibold">Event Photos</h1>
-                        <div class="line"></div>
-                        <a href="https://icsec2024photo.computing.kku.ac.th/library/albums/asmhng6jnp5rad5b/6-november-2024-icsec2024"><button class="btn btn-primary">Click here: ICSEC2024photo</button></a>
+                        <div class="img-photo" style="display: flex; justify-content: center; align-items: center;" >
+                            <img src="./assets/images/photo/photo1.jpg" alt="" class="w-25 ps-3">
+                            <img src="./assets/images/photo/photo2.jpg" alt="" class="w-25 ps-3">
+                            <img src="./assets/images/photo/photo3.jpg" alt=""class="w-25 ps-3"> 
+                        </div>
+                        <div class="line">
+                           
+                        </div>
+                        <a
+                            href="https://icsec2024photo.computing.kku.ac.th/library/albums/asmhng6jnp5rad5b/6-november-2024-icsec2024"><button
+                                class="btn btn-primary">Click here: ICSEC2024photo</button></a>
                     </div>
                 </div>
-
-
             </div>
         </div>
     </section>
